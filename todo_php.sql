40b38192ae5a7d5764d980ea94cd10859cad517c
UNILORN <b5159@oic.jp>
2016-07-18 17:54:42 +0000

    SQLdump

--- todo_php.sql
+++ todo_php.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.12, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.49, for debian-linux-gnu (armv7l)
 --
 -- Host: localhost    Database: todo_php
 -- ------------------------------------------------------
--- Server version	5.7.12-0ubuntu1.1
+-- Server version	5.5.49-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,12 +24,12 @@ DROP TABLE IF EXISTS `group`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `group` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
+  `name` varchar(45) CHARACTER SET sjis NOT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_group_1_idx` (`user_id`),
   CONSTRAINT `fk_group_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `group` (
 
 LOCK TABLES `group` WRITE;
 /*!40000 ALTER TABLE `group` DISABLE KEYS */;
-INSERT INTO `group` VALUES (1,'test',1),(2,'aaa',1);
+INSERT INTO `group` VALUES (1,'test',1),(2,'aaa',1),(4,'???',16),(5,'???',16),(6,'test',16),(10,'てすと',16);
 /*!40000 ALTER TABLE `group` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,8 +51,8 @@ DROP TABLE IF EXISTS `task`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `task` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `time` varchar(45) DEFAULT NULL,
+  `name` varchar(45) CHARACTER SET sjis NOT NULL,
+  `time` datetime DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `group_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -60,7 +60,7 @@ CREATE TABLE `task` (
   KEY `fk_new_table_2_idx` (`group_id`),
   CONSTRAINT `foreign_group` FOREIGN KEY (`group_id`) REFERENCES `group` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `foreugn_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +69,7 @@ CREATE TABLE `task` (
 
 LOCK TABLES `task` WRITE;
 /*!40000 ALTER TABLE `task` DISABLE KEYS */;
-INSERT INTO `task` VALUES (1,'hogehoge','2016-04-25',1,1),(2,'hoge2',NULL,1,1),(3,'hogeeee',NULL,1,2);
+INSERT INTO `task` VALUES (1,'hogehoge','2016-04-25 00:00:00',1,1),(2,'hoge2',NULL,1,1),(3,'hogeeee',NULL,1,2),(8,'あｄｓｄ','2016-07-07 02:02:00',1,1),(9,'本を買う','2016-07-05 05:50:00',1,1);
 /*!40000 ALTER TABLE `task` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +88,7 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,7 +97,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'unilorn','shr850','aaa@oic.jp'),(2,'mikiya','12345','b5159@oic.jp'),(11,'chanu','12345','aa@s.s'),(13,'nucha','chanu','chanu@yahoo.co.jp'),(14,'aaa','aaa','aaa@aaa');
+INSERT INTO `user` VALUES (1,'unilorn','shr850','aaa@oic.jp'),(2,'mikiya','12345','b5159@oic.jp'),(11,'chanu','12345','aa@s.s'),(13,'nucha','chanu','chanu@yahoo.co.jp'),(14,'aaa','aaa','aaa@aaa'),(15,'test','aaa','aa@aa'),(16,'serori','19970110','@aaa');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -110,4 +110,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-07-16 21:23:52
+-- Dump completed on 2016-07-18 17:54:05
